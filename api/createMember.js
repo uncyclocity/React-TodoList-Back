ff36dd3aca555e09cdae6106a6f7afdd8d587964
YSB_uncyclocity <seongbeom_lee@kakao.com>
2021-11-19 17:37:27 +0900

    [UPDATE] 유저 중복생성 되지 않도록 설정

--- api/createMember.js
+++ api/createMember.js
@@ -19,6 +19,13 @@ const createNewPlatform = async (platform) => {
   return new MembersInfo(newPlatform);
 };
 
+const getIsExistMember = (userPlatformDB, id) => {
+  const targetMemberIdx = userPlatformDB.members.findIndex(
+    (member) => member.memberId === id.toString()
+  );
+  return targetMemberIdx >= 0;
+};
+
 const createNewMember = async (memberId, nickname, userPlatformDB) => {
   const userObj = {
     memberId,
@@ -37,9 +44,14 @@ const handler = async (req, res) => {
         if (userPlatformDB === null) {
           userPlatformDB = await createNewPlatform(platform);
         }
-        await createNewMember(id, nickname, userPlatformDB);
-        const usercreated = await userPlatformDB.save();
-        return res.status(200).send(usercreated);
+        if (getIsExistMember(userPlatformDB, id)) {
+          // DB에 존재하는 사용자의 경우 새로 만들지 않음
+          return res.status(200);
+        } else {
+          await createNewMember(id, nickname, userPlatformDB);
+          const usercreated = await userPlatformDB.save();
+          return res.status(200).send(usercreated);
+        }
       } catch (error) {
         return res.status(500).send(error.message);
       }
@@ -48,7 +60,6 @@ const handler = async (req, res) => {
       res.status(422).send("data_incomplete");
     }
   } else {
-    console.log("ㅂㅇ");
     res.status(422).send("req_method_not_supported");
   }
 };
